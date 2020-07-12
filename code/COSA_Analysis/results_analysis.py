111352873338222cbb4072e643cbbbe8fd2f8fb0
anunezji <alejandro.n.j@live.com>
2020-07-12 22:32:15 +0200

    prepared analysis of results to compute outcomes per scenario using new policy parameters

--- code/COSA_Analysis/results_analysis.py
+++ code/COSA_Analysis/results_analysis.py
@@ -22,7 +22,7 @@ from matplotlib.lines import Line2D
 files_dir = os.path.dirname(__file__)
 
 # Set directory with data files
-data_subfolder = 'code\\COSA_Outputs\\'
+data_subfolder = 'code\\COSA_Outputs\\2_results\\'
 input_subfolder = 'code\\COSA_Data\\'
 data_dir = files_dir[:files_dir.rfind('code')] + data_subfolder
 input_dir = files_dir[:files_dir.rfind('code')] + input_subfolder
@@ -30,27 +30,22 @@ input_dir = files_dir[:files_dir.rfind('code')] + input_subfolder
 # Create container dictionaries to import simulation results
 model_files = {}
 com_files = {}
-ag_files = {}
+#ag_files = {}
 
 # Create a dictionary of file name and container dict
-out_dict = {"model_vars_":model_files, "com_formed_":com_files, "agent_vars_":ag_files
-}
+out_dict = {"model_vars_":model_files, "com_formed_":com_files}#"agent_vars_":ag_files}
 
 # Loop through container dictionaries
 for key, val in out_dict.items():
     for input_file in glob.glob(join(data_dir,'*'+key+'.feather')):
 
         # Create a label from the name of the experiment that created the data
-        label = "_".join(input_file.split("_")[4:7])
+        label = "_".join(input_file.split("_")[5:])
+        label = label.replace("_el2yr_"+key+'.feather',"")
         
         # Read data and store it in the container dictionary
         with open(input_file, "r") as mydata:
             val[label] = feather.read_dataframe(input_file)
-
-# List of parameters included in the scenario labels
-cal_pars = ["w_econ","w_swn","w_att","w_subplot","threshold","reduction","awareness_mean","awareness_stdev","awareness_minergie"]
-
-pars_d = {"cal_label":cal_pars}
 
 # Rename second column in analysed files to "variable"
 for key, df in out_dict["model_vars_"].items():
@@ -59,19 +54,15 @@ for key, df in out_dict["model_vars_"].items():
 # Put all data frames into one
 model_df = pd.concat(out_dict["model_vars_"])
 communities_df = pd.concat(out_dict["com_formed_"])
-agents_df = pd.concat(out_dict["agent_vars_"])
+#agents_df = pd.concat(out_dict["agent_vars_"])
 
-# Rename first column in summaries of calibration results
-for df in [model_df, communities_df, agents_df]:
-#for df in [model_df, communities_df]:
+# Temporary fix
+model_df["com_year"].loc["no_com_no_dm"] = 2050
+model_df["com_year"].loc["com_2019_dm_100"] = 2019
+model_df["com_year"].loc["com_2019_no_dm"] = 2019
 
-    # Create new columns with values of scenario parameters
-    for ix in range(len(cal_pars)):
-
-        df[cal_pars[ix]] = df["cal_label"].str.split('_').str[ix]
-
-        # Make numerical parameters float
-        df[cal_pars[ix]] = pd.to_numeric(df[cal_pars[ix]])
+# Create a scenario label
+model_df["scenario_label"] = np.char.array(model_df["com_year"].values.astype(str))+ "_" + np.char.array(model_df["direct_market"].values.astype(str)) + "_" + np.char.array(model_df["direct_market_th"].values.astype(str))
 
 # Import agents info
 agents_info = pd.read_csv(input_dir+'buildings_info.csv', sep=",")
@@ -91,43 +82,39 @@ n_ticks =  len(set(model_df["sim_year"]))
 n_runs = len(set(model_df["run"]))
 
 sc_results = {}
-for cal_lab in list(set(model_df["cal_label"])):
-    sc_results[cal_lab] = {}
+for sc_lab in list(set(model_df["scenario_label"])):
+    sc_results[sc_lab] = {}
 
     for var in vars:
-        sc_results[cal_lab][var] = pd.DataFrame(np.zeros((n_ticks, n_runs)),
+        sc_results[sc_lab][var] = pd.DataFrame(np.zeros((n_ticks, n_runs)),
         index=range(n_ticks), columns=range(n_runs))
         
         for run in range(n_runs):
-            cond = (model_df["cal_label"]==cal_lab) & (model_df["run"]==run)
+            cond = (model_df["scenario_label"]==sc_lab) & (model_df["run"]==run)
             
-            sc_results[cal_lab][var][run] = model_df[var].loc[cond].values
+            sc_results[sc_lab][var][run] = model_df[var].loc[cond].values
 
-            """
-            Take care of duplicated runs
-            """
-
-            sc_results[cal_lab][var][run] = pd.to_numeric(sc_results[cal_lab][var][run])
+            sc_results[sc_lab][var][run] = pd.to_numeric(sc_results[sc_lab][var][run])
         
         # Compute percentiles
-        p5 = sc_results[cal_lab][var].quantile(q=0.05, axis="columns")
-        p50 = sc_results[cal_lab][var].quantile(q=0.5, axis="columns")
-        p95 = sc_results[cal_lab][var].quantile(q=0.95, axis="columns")
+        p5 = sc_results[sc_lab][var].quantile(q=0.05, axis="columns")
+        p50 = sc_results[sc_lab][var].quantile(q=0.5, axis="columns")
+        p95 = sc_results[sc_lab][var].quantile(q=0.95, axis="columns")
 
         # Store percentiles
-        sc_results[cal_lab][var]["p05"] = p5
-        sc_results[cal_lab][var]["p50"] = p50
-        sc_results[cal_lab][var]["p95"] = p95
+        sc_results[sc_lab][var]["p05"] = p5
+        sc_results[sc_lab][var]["p50"] = p50
+        sc_results[sc_lab][var]["p95"] = p95
 
         # Compute and store 90% confidence interval
-        sc_results[cal_lab][var]["CI90"] = sc_results[cal_lab][var]["p95"] - sc_results[cal_lab][var]["p05"]
+        sc_results[sc_lab][var]["CI90"] = sc_results[sc_lab][var]["p95"] - sc_results[sc_lab][var]["p05"]
 
-    sc_results[cal_lab] = pd.concat(sc_results[cal_lab].values(), 
-                            keys=sc_results[cal_lab].keys())
+    sc_results[sc_lab] = pd.concat(sc_results[sc_lab].values(), 
+                            keys=sc_results[sc_lab].keys())
 
-    sc_results[cal_lab] = sc_results[cal_lab].reset_index()
+    sc_results[sc_lab] = sc_results[sc_lab].reset_index()
 
-    sc_results[cal_lab].rename(columns={"level_0":"variable", "level_1":"sim_year"}, inplace=True)
+    sc_results[sc_lab].rename(columns={"level_0":"variable", "level_1":"sim_year"}, inplace=True)
 
 sc_results_analysed = pd.concat(sc_results.values(), keys=sc_results.keys(), names=["scenario"]).reset_index(level=1, drop=True)
 #%% PLOT INSTALLATIONS
@@ -137,39 +124,48 @@ plotvars = ["inst_cum_ind", "inst_cum_com"]
 
 color_d = {"inst_cum_ind":"blue", "inst_cum_com":"red"}
 
-labels_d = {"inst_cum_ind":"Individual installations", "inst_cum_com":"Commnuity installations"}
+labels_d = {"inst_cum_ind":"Individual installations", "inst_cum_com":"Community installations"}
 
-# Create figure
-fig_inst, ax_inst = plt.subplots(1,1, figsize=(6.5,4))
+for scenario in set(sc_results_analysed.index):
 
-# Select data
-plot_df = sc_results_analysed
+    # Create figure
+    fig_inst, ax_inst = plt.subplots(1,1, figsize=(6.5,4))
 
-# Define time variable
-x = range(len(set(plot_df["sim_year"])))
+    # Select data
+    plot_df = sc_results_analysed.loc[scenario]
 
-# Loop through variables
-for var in plotvars:
+    # Define time variable
+    x = range(len(set(plot_df["sim_year"])))
 
-    # Define variable to plot
-    cond_var = plot_df["variable"]==var
+    # Loop through variables
+    for var in plotvars:
 
-    # Plot median
-    ax_inst.plot(plot_df["p50"].loc[cond_var].values, color=color_d[var], label=labels_d[var])
+        # Define variable to plot
+        cond_var = plot_df["variable"]==var
 
-    # Plot confidence interval
-    ax_inst.fill_between(x,plot_df["p05"].loc[cond_var].values,plot_df["p95"].loc[cond_var].values, color=color_d[var], alpha=0.5)
+        # Plot median
+        ax_inst.plot(plot_df["p50"].loc[cond_var].values, color=color_d[var], label=labels_d[var])
 
-    # Set Y-axis label
-    ax_inst.set_ylabel("Cumulative installed capacity [kWp]")
+        # Plot confidence interval
+        ax_inst.fill_between(x,plot_df["p05"].loc[cond_var].values,plot_df["p95"].loc[cond_var].values, color=color_d[var], alpha=0.5)
 
-    # Set X-axis labels
-    d_yrs = 3
-    ax_inst.set_xticks(np.arange(0,len(x),d_yrs))
-    ax_inst.set_xticklabels(np.arange(min(model_df["sim_year"]),max(model_df["sim_year"])+1,d_yrs))
+        # Set Y-axis label
+        ax_inst.set_ylabel("Cumulative installed capacity [kWp]")
 
-# Add legend
-ax_inst.legend(loc="upper left")
+        # Set X-axis labels
+        d_yrs = 3
+        ax_inst.set_xticks(np.arange(0,len(x),d_yrs))
+        ax_inst.set_xticklabels(np.arange(min(model_df["sim_year"]),max(model_df["sim_year"])+1,d_yrs))
+
+        ax_inst.plot(plot_df["p50"].loc[plot_df["variable"]=="inst_cum_ind"].values+plot_df["p50"].loc[plot_df["variable"]=="inst_cum_com"].values, color="k")
+
+    ax_inst.set_ylim(0,20000)
+
+    # Add legend
+    ax_inst.legend(loc="upper left")
+
+    # Add scenario title
+    fig_inst.suptitle(scenario)
 
 #%% EXPORT FIGURE
 fig_inst.savefig(files_dir +"\\fig_inst.svg", format="svg")
@@ -183,37 +179,43 @@ color_d = {"pol_cost_sub_ind":"blue", "pol_cost_sub_com":"red"}
 
 labels_d = {"pol_cost_sub_ind":"Individual installations", "pol_cost_sub_com":"Commnuity installations"}
 
-# Create figure
-fig_inst, ax_inst = plt.subplots(1,1, figsize=(6.5,4))
+for scenario in set(sc_results_analysed.index):
 
-# Select data
-plot_df = sc_results_analysed
+    # Create figure
+    fig_inst, ax_inst = plt.subplots(1,1, figsize=(6.5,4))
 
-# Define time variable
-x = range(len(set(plot_df["sim_year"])))
+    # Select data
+    plot_df = sc_results_analysed.loc[scenario]
 
-# Loop through variables
-for var in plotvars:
+    # Define time variable
+    x = range(len(set(plot_df["sim_year"])))
 
-    # Define variable to plot
-    cond_var = plot_df["variable"]==var
+    # Loop through variables
+    for var in plotvars:
 
-    # Plot median
-    ax_inst.plot(plot_df["p50"].loc[cond_var].values/1000, color=color_d[var], label=labels_d[var])
+        # Define variable to plot
+        cond_var = plot_df["variable"]==var
 
-    # Plot confidence interval
-    ax_inst.fill_between(x,plot_df["p05"].loc[cond_var].values/1000,plot_df["p95"].loc[cond_var].values/1000, color=color_d[var], alpha=0.5)
+        # Plot median
+        ax_inst.plot(plot_df["p50"].loc[cond_var].values/1000, color=color_d[var], label=labels_d[var])
 
-    # Set Y-axis label
-    ax_inst.set_ylabel("Cumulative policy costs [kCHF]")
+        # Plot confidence interval
+        ax_inst.fill_between(x,plot_df["p05"].loc[cond_var].values/1000,plot_df["p95"].loc[cond_var].values/1000, color=color_d[var], alpha=0.5)
 
-    # Set X-axis labels
-    d_yrs = 3
-    ax_inst.set_xticks(np.arange(0,len(x),d_yrs))
-    ax_inst.set_xticklabels(np.arange(min(model_df["sim_year"]),max(model_df["sim_year"])+1,d_yrs))
+        # Set Y-axis label
+        ax_inst.set_ylabel("Cumulative policy costs [kCHF]")
 
-# Add legend
-ax_inst.legend(loc="upper left")
+        # Set X-axis labels
+        d_yrs = 3
+        ax_inst.set_xticks(np.arange(0,len(x),d_yrs))
+        ax_inst.set_xticklabels(np.arange(min(model_df["sim_year"]),max(model_df["sim_year"])+1,d_yrs))
+
+        ax_inst.plot(plot_df["p50"].loc[plot_df["variable"]=="pol_cost_sub_ind"].values/1000+plot_df["p50"].loc[plot_df["variable"]=="pol_cost_sub_com"].values/1000, color="k")
+
+    ax_inst.set_ylim(0,4000)
+
+    # Add legend
+    ax_inst.legend(loc="upper left")
 
 #%% EXPORT FIGURE
 fig.savefig(files_dir +"\\fig.svg", format="svg")
